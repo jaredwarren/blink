165e7eb0a178f7b68f2382d7e95d9268cb0f9aad
Jared Warren <jlwarren1@gmail.com>
2020-12-18 18:26:51 -0700

    testing

--- tester/main.go
+++ tester/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 
 	. "github.com/logrusorgru/aurora"
@@ -12,16 +13,216 @@ const (
 	NumLed = 150
 )
 
-func main() {
-	// fmt.Println("Hello,", Magenta("Aurora"))
-	// fmt.Println(Bold(Red(".")))
+// Printer ...
+type Printer interface {
+	Print(Frame)
+}
 
-	c1 := []string{"red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green"}
-	c2 := []string{"red", "red", "green", "white"}
+// Strand ...
+type Strand struct {
+	Name    string
+	Reverse bool
+	NumLeds int
+	offset  int
+	Printer
+}
 
-	print(c1, c2)
+// Render ...
+func (s *Strand) Render(f Frame) {
+
+	if s.Reverse {
+		for i, j := 0, len(f)-1; i < j; i, j = i+1, j-1 {
+			f[i], f[j] = f[j], f[i]
+		}
+	}
+	s.Print(f)
+
+	// // assume len p < num leds
+	// out := make([]string, s.NumLeds)
+
+	// if s.Reverse {
+	// 	for i := 0; i < s.NumLeds; i++ {
+
+	// 		// for _, f := range p {
+	// 		// 	if i >= s.NumLeds {
+	// 		// 		break
+	// 		// 	}
+	// 		// 	out[i] = f
+	// 		// 	// dev.Leds(0)[i+s.offset] = f
+	// 		// 	i++
+	// 		// }
+	// 	}
+	// } else {
+	// 	for i := s.NumLeds - 1; i >= 0; i-- {
+	// 		for _, f := range p {
+	// 			if i < 0 {
+	// 				break
+	// 			}
+	// 			// dev.Leds(0)[i+s.offset] = f
+	// 			out[i] = f
+	// 			i--
+	// 		}
+	// 	}
+	// }
+
+	// for _, c := range out {
+	// 	pp(c)
+	// }
+}
+
+// Pattern ...
+// type Pattern []uint32
+type Pattern []string
+
+// Frame ... len == num leds
+type Frame []string
+
+// Push ...
+func (f *Frame) Push(c string) bool {
+	if len(*f) == cap(*f) {
+		return false
+	}
+	*f = append(*f, c)
+	return true
+}
+
+// PushP ...
+func (f *Frame) PushP(p Pattern) {
+	for _, pp := range p {
+		if ok := f.Push(pp); !ok {
+			return
+		}
+	}
+}
+
+// Fill ...
+func (f *Frame) Fill(p Pattern) {
+	// push until full
+	for len(*f) < cap(*f) {
+		for _, pp := range p {
+			if ok := f.Push(pp); !ok {
+				return
+			}
+		}
+	}
+}
+
+// Print ...
+func (f *Frame) Print() {
+	for _, p := range *f {
+		pp(p)
+	}
 
 }
+
+// NewFrame ...
+func NewFrame(l int) *Frame {
+	f := make(Frame, 0, l)
+	return &f
+}
+
+func main() {
+
+	// ff := make(Frame, 0, 10)
+	// ff.PushP(Pattern{"a", "b", "c"})
+	// ff.PushP(Pattern{"a", "b", "c"})
+	// ff.PushP(Pattern{"a", "b", "c"})
+	// ff.PushP(Pattern{"a", "b", "c"})
+	// ff.PushP(Pattern{"a", "b", "c"})
+	// ff.Fill(Pattern{"a", "b", "c"})
+
+	// for i := 0; i < 14; i++ {
+	// 	fmt.Println(i)
+	// 	ok := ff.Push(fmt.Sprintf("%d", i))
+	// 	fmt.Println(" ", ok)
+	// }
+
+	// fmt.Printf("%+v\n", ff)
+
+	// return
+
+	// fmt.Println("Hello,", Magenta("Aurora"))
+	// fmt.Println(Bold(Red(".")))
+	fs := genWipe(10, Pattern{"red", "white"})
+
+	for _, f := range fs {
+		for _, ff := range f {
+			pp(ff)
+		}
+
+		fmt.Println("")
+	}
+	// fmt.Println(fs)
+
+	fmt.Println()
+
+	{
+		f := NewFrame(100)
+		f.Fill(Pattern{"red", "green", "white"})
+		f.Print()
+	}
+	fmt.Println()
+
+	l := &Strand{
+		Name:    "Left",
+		NumLeds: 300,
+		Reverse: false,
+		Printer: &ConsolePrinter{},
+	}
+	r := &Strand{
+		Name:    "Right",
+		NumLeds: 300,
+		Reverse: true,
+		Printer: &ConsolePrinter{},
+	}
+	fmt.Println("\n---------")
+	{
+		f := NewFrame(100)
+		f.Fill(Pattern{"red", "green", "white"})
+		l.Render(*f)
+	}
+	fmt.Println("\n---------")
+	{
+		f := NewFrame(100)
+		f.Fill(Pattern{"red", "green", "white"})
+		r.Render(*f)
+	}
+	fmt.Println("\n---------")
+
+	// c1 := []string{"red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green", "red", "white", "green"}
+	// c2 := []string{"red", "red", "green", "white"}
+
+	// print(c1, c2)
+
+}
+
+// It would be nice to  "spread out" / repeat frames
+func genWipe(l int, p Pattern) []Frame {
+	frames := make([]Frame, l)
+	for i := 0; i < l; i++ {
+		f := make(Frame, 0, l)
+
+		done := true // there's got to be a bette way
+		max := int(math.Ceil(float64(l) / float64(len(p))))
+		for j := 0; j < max; j++ {
+			if j <= i {
+				f.PushP(p)
+			} else {
+				f.Fill(Pattern{"green"}) // TODO: replace with "nil"
+				done = false
+			}
+		}
+		frames[i] = f
+		if done {
+			break
+		}
+	}
+
+	return frames
+}
+
+//
+//
 
 func print(c1, c2 []string) {
 	// reverse c2
@@ -38,7 +239,7 @@ func print(c1, c2 []string) {
 
 	fmt.Println("")
 	for _, s := range c1 {
-		p(s)
+		pp(s)
 		time.Sleep(100 * time.Millisecond)
 	}
 
@@ -47,7 +248,7 @@ func print(c1, c2 []string) {
 	fmt.Println("\n-----------")
 
 	for _, s := range c2 {
-		p(s)
+		pp(s)
 		time.Sleep(300 * time.Millisecond)
 	}
 
@@ -55,22 +256,37 @@ func print(c1, c2 []string) {
 
 }
 
-func p(x string) {
+// ConsolePrinter console printer
+type ConsolePrinter struct{}
+
+// Print ...
+func (p *ConsolePrinter) Print(f Frame) {
+	for _, p := range f {
+		pp(p)
+	}
+}
+
+func pp(x string) {
 	switch x {
 	case "red":
-		fmt.Print(Bold(Red(".")))
+		fmt.Print(Bold(Red("⬤")))
 	case "green":
-		fmt.Print(Bold(Green(".")))
+		fmt.Print(Bold(Green("⬤")))
+	case "black":
+		fmt.Print(Bold(Black("⬤")))
+	case "off":
+		fmt.Print(Bold(Black("⬤")))
 	case "white":
-		fmt.Print(Bold("."))
-
+		fmt.Print(Bold("⬤"))
+	default:
+		fmt.Print(Bold(" "))
 	}
 }
 
 func wipe(color string) {
 	fmt.Print("\r")
 	for i := 0; i < 155; i++ {
-		p(color)
+		pp(color)
 		time.Sleep(50 * time.Millisecond)
 	}
 }
@@ -81,15 +297,15 @@ func redGreen() {
 		for i := 0; i < 155; i++ {
 			if j%2 == 0 {
 				if i%2 == 0 {
-					p("red")
+					pp("red")
 				} else {
-					p("green")
+					pp("green")
 				}
 			} else {
 				if i%2 == 0 {
-					p("green")
+					pp("green")
 				} else {
-					p("red")
+					pp("red")
 				}
 			}
 		}
@@ -104,27 +320,27 @@ func redWhiteGreen() {
 		for i := 0; i < 155; i++ {
 			if j%3 == 0 {
 				if i%3 == 0 {
-					p("red")
+					pp("red")
 				} else if i%3 == 1 {
-					p("white")
+					pp("white")
 				} else {
-					p("green")
+					pp("green")
 				}
 			} else if j%3 == 1 {
 				if i%3 == 0 {
-					p("green")
+					pp("green")
 				} else if i%3 == 1 {
-					p("red")
+					pp("red")
 				} else {
-					p("white")
+					pp("white")
 				}
 			} else {
 				if i%3 == 0 {
-					p("white")
+					pp("white")
 				} else if i%3 == 1 {
-					p("green")
+					pp("green")
 				} else {
-					p("red")
+					pp("red")
 				}
 			}
 		}
--- ws2811/main.go
+++ ws2811/main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"net/http"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/jaredwarren/app"
 	ws2811 "github.com/rpi-ws281x/rpi-ws281x-go"
-	"github.com/spf13/cobra"
 )
 
 const (
@@ -21,33 +17,46 @@ const (
 
 // Strand ...
 type Strand struct {
+	Name    string
 	Reverse bool
 	NumLeds int
+	offset  int
 }
 
-// Frame ...
-type Frame struct {
-	Pattern []uint32
-}
-
-// // Device should match https://pkg.go.dev/github.com/rpi-ws281x/rpi-ws281x-go#MakeWS2811
-// type Device interface {
-// 	Fini()
-// 	Init() error
-// 	Leds(channel int) []uint32
-// 	Render() error
-// 	SetBrightness(channel int, brightness int)
-// 	SetLedsSync(channel int, leds []uint32) error
-// 	Wait() error
-// }
-
-func checkError(err error) {
-	if err != nil {
-		panic(err)
+// Render ...
+func (s *Strand) Render(p Pattern) {
+	// assume len p < num leds
+	if s.Reverse {
+		for i := 0; i < s.NumLeds; i++ {
+			for _, f := range p {
+				if i >= s.NumLeds {
+					break
+				}
+				dev.Leds(0)[i+s.offset] = f
+				i++
+			}
+		}
+	} else {
+		for i := 0; i < s.NumLeds; i++ {
+			for _, f := range p {
+				if i >= s.NumLeds {
+					break
+				}
+				dev.Leds(0)[i+s.offset] = f
+				i++
+			}
+		}
 	}
 }
 
-var wg sync.WaitGroup
+// Pattern ...
+type Pattern []uint32
+
+var (
+	wg      sync.WaitGroup
+	dev     *ws2811.WS2811
+	strands []*Strand
+)
 
 func main() {
 	fmt.Println("Starting....")
@@ -56,7 +65,8 @@ func main() {
 	opt.Channels[0].Brightness = brightness
 	opt.Channels[0].LedCount = ledCounts
 
-	dev, err := ws2811.MakeWS2811(&opt)
+	var err error
+	dev, err = ws2811.MakeWS2811(&opt)
 	if err != nil {
 		panic(err)
 	}
@@ -67,185 +77,81 @@ func main() {
 	}
 	defer dev.Fini()
 
-	// remove after testing
-	s := Server{dev}
-	s.run(nil, nil)
+	//
+	// setup strands
+	//
+	strands = []*Strand{
+		&Strand{
+			Name:    "Left",
+			NumLeds: 300,
+			Reverse: false,
+		},
+		&Strand{
+			Name:    "Right",
+			NumLeds: 300,
+			Reverse: true,
+		},
+	}
+
+	//
+	// Run
+	//
+	run()
 
 	wg.Wait()
-
-	return
-
-	// setup server
-	conf := &app.WebConfig{
-		Host: ":8084",
-	}
-	a := app.NewWeb(conf)
-
-	Register(a, dev)
-	fmt.Println("Ready....")
-	d := <-a.Exit
-	fmt.Printf("Done:%+v\n", d)
-
-	return
-	// old cmd stuff
-
-	rootCmd := &cobra.Command{
-		Use:   "clear",
-		Short: "Clear Everything",
-		Long:  `clear`,
-		Run: func(cmd *cobra.Command, args []string) {
-			for i := 0; i < ledCounts; i++ {
-				dev.Leds(0)[i] = 0x000000
-			}
-			checkError(dev.Render())
-		},
-	}
-	rootCmd.AddCommand(&cobra.Command{
-		Use:   "show",
-		Short: "show",
-		Long:  `show`,
-		Run: func(cmd *cobra.Command, args []string) {
-			for {
-				chase(dev)
-			}
-
-		},
-	})
-
-	rootCmd.AddCommand(&cobra.Command{
-		Use:   "clear",
-		Short: "Clear Everything",
-		Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-		Run: func(cmd *cobra.Command, args []string) {
-			clear(dev)
-		},
-	})
-	rootCmd.Execute()
 }
 
-func chase(dev *ws2811.WS2811) {
-	for i := 0; i < ledCounts; i++ {
-		if i%2 == 0 {
-			dev.Leds(0)[i] = 0xff007f
-		} else if i%3 == 0 {
-			dev.Leds(0)[i] = 0xff007f
-		} else {
-			dev.Leds(0)[i] = 0xff007f
-		}
-
-		checkError(dev.Render())
-		time.Sleep(1 * time.Millisecond)
+func render() {
+	for _, s := range strands {
+		s.Render()
 	}
+	dev.Render()
+}
+
+func clear() {
 	for i := 0; i < ledCounts; i++ {
 		dev.Leds(0)[i] = 0x000000
 	}
-	checkError(dev.Render())
+	dev.Render()
 }
 
-func clear(dev *ws2811.WS2811) {
-	for i := 0; i < ledCounts; i++ {
-		dev.Leds(0)[i] = 0x000000
-	}
-	checkError(dev.Render())
-}
-
-func altrg(dev *ws2811.WS2811) {
-	for i := 0; i < ledCounts; i++ {
-		if i%2 == 0 {
-			dev.Leds(0)[i] = 0xff0000
-		} else {
-			dev.Leds(0)[i] = 0x000000
-		}
-	}
-	checkError(dev.Render())
-	time.Sleep(200 * time.Millisecond)
-	for i := 0; i < ledCounts; i++ {
-		if i%2 == 0 {
-			dev.Leds(0)[i] = 0x000000
-		} else {
-			dev.Leds(0)[i] = 0x00ff00
-		}
-	}
-	checkError(dev.Render())
-	time.Sleep(200 * time.Millisecond)
-}
-
-// Server ..
-type Server struct {
-	dev *ws2811.WS2811
-}
-
-// Register ...
-func Register(a *app.Service, dev *ws2811.WS2811) {
-	m := a.Mux
-
-	s := Server{dev}
-
-	m.HandleFunc("/", home).Methods("GET")
-	m.HandleFunc("/clear", s.clear).Methods("GET")
-	m.HandleFunc("/run", s.run).Methods("GET")
-
-}
-
-// Close handler.
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("~~~ Home ~~~")
-	tmpl := template.Must(template.ParseFiles("layout.html", "base.html"))
-	tmpl.ExecuteTemplate(w, "base", struct {
-		PageTitle string
-	}{
-		"Something",
-	})
-}
-
-func (s *Server) clear(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("~~~ clear ~~~")
-	for i := 0; i < ledCounts; i++ {
-		s.dev.Leds(0)[i] = 0x000000
-	}
-	s.dev.Render()
-	fmt.Fprintf(w, "clear")
-}
-
-func (s *Server) run(w http.ResponseWriter, r *http.Request) {
+func run() {
 	fmt.Println("~~~ run ~~~")
 
 	wg.Add(1)
 	go func() {
-		clear(s.dev)
+		clear()
 
-		// s.setRG()
+		// setRG()
 
-		// s.wipe("red")
-		// s.wipe("green")
-		// s.wipe("white")
+		// wipe("red")
+		// wipe("green")
+		// wipe("white")
 		wg.Done()
 	}()
 
-	// go s.redGreen()
-	// go s.redWhiteGreen()
+	// go redGreen()
+	// go redWhiteGreen()
 	return
 
 	go func() {
 		for i := 0; i < ledCounts; i++ {
 			if i%2 == 0 {
-				s.dev.Leds(0)[i] = 0xff0000
+				dev.Leds(0)[i] = 0xff0000
 			} else if i%3 == 0 {
-				s.dev.Leds(0)[i] = 0x00ff00
+				dev.Leds(0)[i] = 0x00ff00
 			} else {
-				s.dev.Leds(0)[i] = 0x000000
+				dev.Leds(0)[i] = 0x000000
 			}
 
-			s.dev.Render()
+			dev.Render()
 			time.Sleep(1 * time.Millisecond)
 		}
 		fmt.Println("clearing")
 		for i := 0; i < ledCounts; i++ {
-			s.dev.Leds(0)[i] = 0x000000
+			dev.Leds(0)[i] = 0x000000
 		}
-		s.dev.Render()
+		dev.Render()
 		fmt.Println("done")
 	}()
 
@@ -258,54 +164,54 @@ func (s *Server) run(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "running...")
 }
 
-func (s *Server) setRG() {
+func setRG() {
 	for i := 0; i < 300; i++ {
 		if everyOther && i%2 != 0 {
-			s.setC1(i, "black")
-			s.setC2(i, "black")
+			setC1(i, "black")
+			setC2(i, "black")
 			continue
 		}
 
 		if i%4 == 0 {
-			s.setC1(i, "red")
-			s.setC2(i, "red")
+			setC1(i, "red")
+			setC2(i, "red")
 		} else {
-			s.setC1(i, "green")
-			s.setC2(i, "green")
+			setC1(i, "green")
+			setC2(i, "green")
 		}
 	}
-	s.dev.Render()
+	dev.Render()
 }
 
-func (s *Server) wipe(color string) {
+func wipe(color string) {
 	for i := 0; i < 300; i++ {
 		if everyOther && i%2 == 0 {
-			s.setC1(i, "black")
-			s.setC2(i, "black")
+			setC1(i, "black")
+			setC2(i, "black")
 			continue
 		}
 
 		// fmt.Println("set:", i, color)
 
-		s.setC1(i, color)
-		s.setC2(i, color)
-		s.dev.Render()
+		setC1(i, color)
+		setC2(i, color)
+		dev.Render()
 		time.Sleep(100 * time.Millisecond)
 	}
 }
 
 var everyOther = true
 
-func (s *Server) setC1(i int, color string) {
+func setC1(i int, color string) {
 	i = i + ledPerStrand
 	// fmt.Println("  1:", i)
-	s.dev.Leds(0)[i] = getHex(color)
+	dev.Leds(0)[i] = getHex(color)
 }
 
-func (s *Server) setC2(i int, color string) {
+func setC2(i int, color string) {
 	i = ledPerStrand - i - 1
 	// fmt.Println("  2:", i)
-	s.dev.Leds(0)[i] = getHex(color)
+	dev.Leds(0)[i] = getHex(color)
 }
 
 func getHex(cs string) uint32 {
@@ -322,68 +228,68 @@ func getHex(cs string) uint32 {
 	return 0xffffff
 }
 
-func (s *Server) redWhiteGreen() {
+func redWhiteGreen() {
 	for j := 0; j < 1000; j++ {
 		for i := 0; i < 600; i++ {
 			if j%3 == 0 {
 				if i%3 == 0 {
 					// p("red")
-					s.dev.Leds(0)[i] = 0xff0000
+					dev.Leds(0)[i] = 0xff0000
 				} else if i%3 == 1 {
-					s.dev.Leds(0)[i] = 0xffffff
+					dev.Leds(0)[i] = 0xffffff
 					// p("white")
 				} else {
-					s.dev.Leds(0)[i] = 0x00ff00
+					dev.Leds(0)[i] = 0x00ff00
 					// p("green")
 				}
 			} else if j%3 == 1 {
 				if i%3 == 0 {
 					// p("green")
-					s.dev.Leds(0)[i] = 0x00ff00
+					dev.Leds(0)[i] = 0x00ff00
 				} else if i%3 == 1 {
 					// p("red")
-					s.dev.Leds(0)[i] = 0xff0000
+					dev.Leds(0)[i] = 0xff0000
 				} else {
 					// p("white")
-					s.dev.Leds(0)[i] = 0xffffff
+					dev.Leds(0)[i] = 0xffffff
 				}
 			} else {
 				if i%3 == 0 {
 					// p("white")
-					s.dev.Leds(0)[i] = 0xffffff
+					dev.Leds(0)[i] = 0xffffff
 				} else if i%3 == 1 {
 					// p("green")
-					s.dev.Leds(0)[i] = 0x00ff00
+					dev.Leds(0)[i] = 0x00ff00
 				} else {
 					// p("red")
-					s.dev.Leds(0)[i] = 0xff0000
+					dev.Leds(0)[i] = 0xff0000
 				}
 			}
 		}
-		s.dev.Render()
+		dev.Render()
 		time.Sleep(200 * time.Millisecond)
 		// fmt.Print("\r")
 	}
 }
 
-func (s *Server) redGreen() {
+func redGreen() {
 	for j := 0; j < 1000; j++ {
 		for i := 0; i < 600; i++ {
 			if j%2 == 0 {
 				if i%2 == 0 {
-					s.dev.Leds(0)[i] = 0xff0000
+					dev.Leds(0)[i] = 0xff0000
 				} else {
-					s.dev.Leds(0)[i] = 0x00ff00
+					dev.Leds(0)[i] = 0x00ff00
 				}
 			} else {
 				if i%2 == 0 {
-					s.dev.Leds(0)[i] = 0x00ff00
+					dev.Leds(0)[i] = 0x00ff00
 				} else {
-					s.dev.Leds(0)[i] = 0xff0000
+					dev.Leds(0)[i] = 0xff0000
 				}
 			}
 		}
-		s.dev.Render()
+		dev.Render()
 		time.Sleep(200 * time.Millisecond)
 	}
 }
